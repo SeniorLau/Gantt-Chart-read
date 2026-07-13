8e710dfc640ee55710fa1128fca0f2be84f5fa91
SeniorLau <Laurens.Leys@hotmail.com>
2026-07-13 16:30:42 +0200

    Remove DataFrame display after parsing Excel
    Removed display of specific DataFrame columns after parsing Excel.

--- app.py
+++ app.py
@@ -18,13 +18,7 @@ uploaded = st.file_uploader(
 if uploaded:
 
     df = parse_excel(uploaded)
-    st.write(
-    df[[
-        "Task",
-        "Progress_raw",
-        "Progress"
-    ]]
-)
+
 
     if df.empty:
         st.error("No tasks detected in Excel. Check the template structure.")
